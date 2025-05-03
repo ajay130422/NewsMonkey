02fcdc8fedad7a6123e705e220afb27f0f74f57b
ajay130422 <ajnaman13@gmail.com>
2025-05-04 00:35:35 +0530

    first commit

---
+++ src/components/Newsboard.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { useState } from "react"
+import NewsItem from "./NewsItem";
+
+
+const Newsboard = ({category}) => {
+
+
+    const [articles, setArticles] = useState([]);
+    // useEffect(()=>{
+    //     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+    //     fetch(url).then(response=>response.json()).then(data=> setArticles(data.articles));
+
+    // } , [])
+
+    useEffect(() => {
+        const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
+        fetch(url)
+          .then((response) => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+          })
+          .then((data) => setArticles(data.articles))
+          .catch((error) => console.error("API Fetch Error:", error));
+      }, [category]);
+      
+
+  return (
+    <div>
+      <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+      {articles.map((news,index)=>{
+        return <NewsItem key={index} title={news.title} descriptoion={news.description} src={news.urlToImage} url={news.url}/>
+      })}
+    </div>
+  )
+}
+
+export default Newsboard

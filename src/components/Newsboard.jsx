fcff3bc5450b93b304fa48900fc30b5378fd90ae
ajay130422 <ajnaman13@gmail.com>
2025-06-26 16:31:25 +0530

    changeee

--- src/components/Newsboard.jsx
+++ src/components/Newsboard.jsx
@@ -12,9 +12,7 @@ const Newsboard = ({category}) => {
     //     fetch(url).then(response=>response.json()).then(data=> setArticles(data.articles));
 
     // } , [])
-
     useEffect(() => {
-      console.log("API Key:", process.env.VITE_API_KEY);
         const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
         fetch(url)
           .then((response) => {
